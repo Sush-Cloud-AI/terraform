7a50843c1162262e4c8bfceb60317f4ba919a597
Sush <sushcloud12@gmail.com>
2023-04-10 23:29:20 +0100

    i

--- 02-vars/demo.tf
+++ 02-vars/demo.tf
@@ -11,3 +11,29 @@ output "sample-op1" {
     value = "value is ${var.sample}"
   
 }
+
+## default varible type
+variable "number" {
+    default = 100
+
+}
+
+output "number" {
+  value = var.number
+
+}
+## list variable
+variable "list-ex" {
+    default = [
+        "DevOps",
+        100,
+        false,
+        "verma"
+    ]
+  
+}
+
+output "list-ex" {
+    value = "Welcome to ${var.list-ex[0]} Training , Trainer Name is ${var.list-ex[3]} and the training duration is${var.list-ex[1]} hours "
+  
+}
