16ef4de31cba4eb272c2212bbe956a0ce7fb4ae2
Sush <sushcloud12@gmail.com>
2023-04-11 18:18:17 +0100

    i

--- 03-ec2sample/ec2.tf
+++ 03-ec2sample/ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "demo" {
   ami           = "ami-0db68915b3e8a3ce3"  ## argument
   instance_type = "t2.micro"
+  vpc_security_group_ids = [ aws_security_group.allow_ssh.id ]
 
   tags = {
     Name = "first-ec2"
@@ -13,4 +14,32 @@ resource "aws_instance" "demo" {
 output "private_ip" {
     value = aws_instance.demo.private_ip   ## private_ip attribute
   
+}
+
+# Adding security group
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_SSH"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description      = "SSH to VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
 }
