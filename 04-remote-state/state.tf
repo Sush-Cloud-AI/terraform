28d4b68e31a9b0f69fe450da03b57a6518d64fc7
Sush <sushcloud12@gmail.com>
2023-05-15 20:54:57 +0100

    i

--- 04-remote-state/state.tf
+++ 04-remote-state/state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "sush-rem-state-file"
-    key    = "statefile/terraform.tfstate"
+    key    = "statefile1/terraform.tfstate"
     region = "us-east-1"
   }
 }
