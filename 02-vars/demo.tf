b225b058d8485570901a4a37e4aa515f727cd82e
Sush <sushcloud12@gmail.com>
2023-05-15 19:15:30 +0100

    i

--- 02-vars/demo.tf
+++ 02-vars/demo.tf
@@ -12,57 +12,57 @@ output "sample-op1" {
   
 }
 
-## default varible type
-variable "number" {
-    default = 100
+# ## default varible type
+# variable "number" {
+#     default = 100
 
-}
+# }
 
-output "number" {
-  value = var.number
+# output "number" {
+#   value = var.number
 
-}
-## list variable
-variable "list-ex" {
-    default = [
-        "DevOps",
-        100,
-        false,
-        "verma"
-    ]
+# }
+# ## list variable
+# variable "list-ex" {
+#     default = [
+#         "DevOps",
+#         100,
+#         false,
+#         "verma"
+#     ]
   
-}
+# }
 
-output "list-ex" {
-    value = "Welcome to ${var.list-ex[0]} Training , Trainer Name is ${var.list-ex[3]} and the training duration is ${var.list-ex[1]} hours."
+# output "list-ex" {
+#     value = "Welcome to ${var.list-ex[0]} Training , Trainer Name is ${var.list-ex[3]} and the training duration is ${var.list-ex[1]} hours."
 
   
-}
+# }
 
 
-### Map Variable
+# ### Map Variable
 
-variable "map-var" {
-    default = {
-        Class = "Devops",
-        Timing = "7AM",
-        Duration = 85
-    }  
-}
+# variable "map-var" {
+#     default = {
+#         Class = "Devops",
+#         Timing = "7AM",
+#         Duration = 85
+#     }  
+# }
 
-output "map-var" {
-    value = " Welcome to ${var.map-var["Class"]} and the timings are ${var.map-var["Timing"]} and the duaration is ${var.map-var["Duration"]}."
+# output "map-var" {
+#     value = " Welcome to ${var.map-var["Class"]} and the timings are ${var.map-var["Timing"]} and the duaration is ${var.map-var["Duration"]}."
   
-}
+# }
 
-## terraform.tfvars varibales demo
+# ## terraform.tfvars varibales demo
 
-variable "state" {}
-variable "country" {}
+# variable "state" {}
+# variable "country" {}
 
-output "state-country-name" {
-  value = "Name of the state where is reside is ${var.state} and my county is ${var.country}."
-}
+# output "state-country-name" {
+#   value = "Name of the state where is reside is ${var.state} and my county is ${var.country}."
+# }
 
 
 ## terraform apply -auto-approve -var country=Bharath ... passing variables from runtime 
