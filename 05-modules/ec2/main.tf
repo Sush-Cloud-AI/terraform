f4711a0637848ce93dc78a55e41e1b29c58136be
Sush <sushcloud12@gmail.com>
2023-05-17 15:31:27 +0100

    i

---
+++ 05-modules/ec2/main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "demo" {
+  ami           = "ami-0db68915b3e8a3ce3"  ## argument
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ aws_security_group.allow_ssh.id ]
+
+  tags = {
+    Name = "first-ec2"
+    ENV = "dev"
+    Project = "cc"
+    
+  }
+}
