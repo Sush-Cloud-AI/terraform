402a65c155aa5f001a5e766a86db6cb256d9fb4a
Sush <sushcloud12@gmail.com>
2023-05-18 20:06:50 +0100

    i

--- roboshop/main.tf
+++ roboshop/main.tf
@@ -1,9 +1,9 @@
-# module "frontend" {
-#   source = "./ec2"
-#   COMPONENT = "frontend"
-#   APP_VERSION ="0.0.1"
+module "frontend" {
+  source = "./ec2"
+  COMPONENT = "frontend"
+  APP_VERSION ="0.0.1"
 
-# }
+}
 
 # module "mongodb" {
 #   source = "./ec2"
@@ -22,9 +22,9 @@
 
 
 
-module "roboshop" {
-    for_each = var.ALL_COMPONENTS  #  for_each is like a loop will run for all variables delcred
-    source = "./ec2"
-    COMPONENT = each.key
-    APP_VERSION = each.value.app_version
-}
+# module "roboshop" {
+#     for_each = var.ALL_COMPONENTS  #  for_each is like a loop will run for all variables delcred
+#     source = "./ec2"
+#     COMPONENT = each.key
+#     APP_VERSION = each.value.app_version
+# }
