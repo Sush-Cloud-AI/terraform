6484f662b2023298bd85d1dd64ee38851606a63d
Sush <sushcloud12@gmail.com>
2023-05-18 19:16:17 +0100

    i

--- roboshop/main.tf
+++ roboshop/main.tf
@@ -1,4 +1,4 @@
-module "frontend" {
+module "roboshop" {
     for_each = var.ALL_COMPONENTS  #  for_each is like a loop will run for all variables delcred
     source = "./ec2"
     COMPONENT = each.key
