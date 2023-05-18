ab4bf053996846383ff1f08b2cdffbd782b277cb
Sush <sushcloud12@gmail.com>
2023-05-18 09:04:29 +0100

    i

--- 05-modules/ec2/main.tf
+++ 05-modules/ec2/main.tf
@@ -3,6 +3,15 @@ resource "aws_instance" "demo" {
   instance_type = "t2.micro"
   vpc_security_group_ids = [ var.sg]
 
+provisioner "local-exec" {
+    command = <<EOF
+    sleep 60
+    cd /home/centos/ansible
+    ansible-playbook ${self.public_ip} -u centos -e ansible_password=DevOps321 roboshop-push.yml -e COMPONENT=frontend -e TAG_NAME=0.0.1 -e ENV=dev
+    
+    EOF
+  }
+
 }
 
 variable "sg" {
