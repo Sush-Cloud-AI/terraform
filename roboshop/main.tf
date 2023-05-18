ce8508f55f35ceb2c7050d292bcec5dd0a8d2e18
Sush <sushcloud12@gmail.com>
2023-05-18 14:29:46 +0100

    i

--- roboshop/main.tf
+++ roboshop/main.tf
@@ -1,6 +1,13 @@
 module "frontend" {
     source = "./ec2"
     COMPONENT = "frontend"
+    APP_VERSION = "0.0.1" 
+}
+
+
+module "cart" {
+    source = "./ec2"
+    COMPONENT = "cart"
     APP_VERSION = "0.0.1"
   
 }
