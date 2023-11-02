23928ba4943293366f64b00e1ce607e65fbc0ad6
Sush <sushcloud12@gmail.com>
2023-11-02 12:27:47 +0000

    i

--- 03-ec2sample/ec2.tf
+++ 03-ec2sample/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "demo" {
   ami           = "ami-0ef141c0ad615f89f"  ## argument
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "first-ec2"
