9a32ba2ea305d61ede3fde3ffc508be42b719ce2
Sush <sushcloud12@gmail.com>
2023-11-02 12:13:48 +0000

    i

--- 03-ec2sample/ec2.tf
+++ 03-ec2sample/ec2.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "demo" {
-  ami           = "ami-0db68915b3e8a3ce3"  ## argument
+  ami           = "ami-0ef141c0ad615f89f"  ## argument
   instance_type = "t2.micro"
-  vpc_security_group_ids = [ aws_security_group.allow_ssh.id ]
 
   tags = {
     Name = "first-ec2"
@@ -9,37 +8,37 @@ resource "aws_instance" "demo" {
     Project = "cc"
     
   }
-}
+# }
 
-output "private_ip" {
-    value = aws_instance.demo.private_ip   ## private_ip attribute
+# output "private_ip" {
+#     value = aws_instance.demo.private_ip   ## private_ip attribute
   
-}
+# }
 
-# Adding security group
+# # Adding security group
 
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_SSH"
-  description = "Allow SSH inbound traffic"
+# resource "aws_security_group" "allow_ssh" {
+#   name        = "allow_SSH"
+#   description = "Allow SSH inbound traffic"
 
-  ingress {
-    description      = "SSH to VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+#   ingress {
+#     description      = "SSH to VPC"
+#     from_port        = 22
+#     to_port          = 22
+#     protocol         = "tcp"
+#     cidr_blocks      = ["0.0.0.0/0"]
 
-  }
+#   }
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+#   egress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
 
-  tags = {
-    Name = "allow_ssh"
-  }
-}
+#   tags = {
+#     Name = "allow_ssh"
+#   }
+# }
